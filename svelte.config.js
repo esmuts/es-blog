49a65544c3b2ced2b7bc2c0a246218dd62f63093
Smuts <eckards@sun.ac.za>
2024-03-26 09:21:51 +0200

    minor tweaks

--- svelte.config.js
+++ svelte.config.js
@@ -7,11 +7,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			images: {
-				formats: ['image/avif', 'image/webp', 'image/jpg', 'image/png']
-			}
-		})
+		adapter: adapter()
 	},
 
 	// Adds .md extensions to Svelte
