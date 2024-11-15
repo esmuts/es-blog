8dab6c101eead0bc50488ff3e02810645362cf56
esmuts <eckardsmuts@gmail.com>
2024-11-15 10:05:57 +0200

    fixing bugs

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,26 @@
+<!-- Renders About page -->
+
+<script>
+	import src from '$lib/images/early-morning-hike.jpg';
+</script>
+
+<p>
+	I'm a Lecturer in the Department of English at Stellenbosch University. My research explores the
+	confluence of social and ecological issues in literary texts, focusing especially on postcolonial
+	writing. Currently, I'm working on a book about alternative temporalities in a range of novels by
+	Southern African authors, including Zoë Wicomb, Bessie Head, Nadine Gordimer, Imraan Coovadia, and
+	J.M. Coetzee. From time to time I write short stories, and I try to never be too far away from the
+	sea.
+</p>
+
+<center><img {src} alt="Eckard Smuts profile pic" /></center>
+
+<center><p>Contact: eckards@sun.ac.za</p></center>
+
+<style>
+	img {
+		margin-top: 1rem;
+		width: 100%;
+		max-width: 340px;
+	}
+</style>
